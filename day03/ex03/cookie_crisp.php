fc389ec3d8ce1f7719076077da2d173805a163d0
gundam1337 <omarderkaoui1337@gmail.com>
2022-10-03 00:39:28 +0100

    day03 apache shit

---
+++ day03/ex03/cookie_crisp.php
@@ -0,0 +1,22 @@
+<?php
+
+//the url = eXrXpX.day03.ex03.42.fr
+if (array_key_exists("action", $_GET))
+{
+    if ($_GET["action"] == "set" && $_GET["name"] != '' && $_GET["value"] != '')
+    {
+        $cookie_name = $_GET['name'];
+        $cookie_value = $_GET['value'];
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), '/'); // 86400 = 1 day
+    }
+    if($_GET['action'] == "get" && $_GET["name"] != '' && $_GET["value"] != '')
+    {
+        $cookie_name = $_GET['name'];
+        print $_COOKIE[$cookie_name];
+    }
+    if($_GET['action'] == "del" && $_GET["name"] != '' && $_GET["value"] != '')
+    {
+        setcookie($tab["name"], '', time() - 3600);   
+    }
+}
+?>
