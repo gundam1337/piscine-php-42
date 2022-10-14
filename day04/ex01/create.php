2f04b480336a5aa14b93bdc39f8fd6f6081ae22c
gundam1337 <omarderkaoui1337@gmail.com>
2022-10-14 01:27:24 +0100

    ex01/day04

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -1,14 +1,62 @@
 <?php
-// page2.php
-
-session_start();
-
-echo 'Welcome to page #2<br />';
-
-echo $_SESSION['favcolor']; // green
-echo $_SESSION['animal'];   // cat
-echo date('Y m d H:i:s', $_SESSION['time']);
-
-// You may want to use SID here, like we did in page1.php
-echo '<br /><a href="page1.php">page 1</a>';
+    if ($_POST['login'] && $_POST['passwd'] && $_POST['submit'] && $_POST['submit'] == "OK")
+    {
+        if (file_exists("../htdocs") != true)
+            mkdir("../htdocs");
+        if (!file_exists('../htdocs/private'))
+            mkdir('../htdocs/private');
+            $users_db = unserialize(file_get_contents('../htdocs/private/passwd'));
+            $if_exist = false;
+            foreach($users_db as $key => $user)
+            {
+                if ($user["login"] === $_POST["login"])
+                    $if_exist = true;
+            }
+        if ($if_exist)
+        {
+            echo "ERROR\n";
+            
+        }
+        else
+        {
+            $users_db[] = array('login' => $_POST['login'], 'passwd' => hash('whirlpool', $_POST['passwd']));
+            file_put_contents('../htdocs/private/passwd', serialize($users_db));
+			echo "OK\n";
+        }  
+    }
+    else
+    {
+        echo "ERROR\n";
+        
+    }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <title>Create account</title>
+
+</head>
+
+<div class="center">
+    
+    <form action="create.php" method="POST">
+      <div class="inputbox">
+        <input type="login" name = "login" >
+        <span>login</span>
+      </div>
+      <div class="inputbox">
+        <input type="password" name="passwd" >
+        <span>passwd</span>
+      </div>
+      <div class="inputbox">
+        <input type="submit" name="submit" value="OK">
+      </div>
+    </form>
+  </div>
+
+
+</body>
+</html>
